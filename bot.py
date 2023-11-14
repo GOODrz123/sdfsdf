11ee9164a9c69da52bf19fed8e7811918aaccd62
GOODrz123 <144367224+GOODrz123@users.noreply.github.com>
2023-11-14 04:20:28 -0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,8 @@
+
+import discord
+from discord.ext import commands
+
+token = "MTE3MjE1ODg5NDczMTE2NTc0Nw.GzjVad.hkga4EstoWi2EczLjrpHTETZvlSKgTg6Hro8_U" 
+intents = discord.Intents.default()
+
+bot.run(token)
